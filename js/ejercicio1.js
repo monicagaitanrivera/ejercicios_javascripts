db4f3b81b2bc9436b0708ac1d5f3bcdd8596289b
monicagaitanrivera <riveramonny123@gmail.com>
2022-02-04 11:05:38 -0500

    subiendo test

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -6,13 +6,10 @@ console.log(mundo);
 let cliente= "Monica"
 console.log("bienvenido,sr",cliente);
 
-/* Solucion que muestre el total de un producto */ 
+/* Solucion que muestre el total de un producto porcentaje */ 
 
 let valorproducto = 32000;
-let  impuesto= 0.10;
+let  impuesto= .10;
 let domici =3000;
 
-var costototal= valorproducto * impuesto + domici;
-console.log("sub-total:" + (valorproducto + impuesto));
-console.log("domicilio:" + domici);
-console.log("El costo total es de: $" + costototal)
+console.log("El costo total es de: $",(( valorproducto * impuesto ) + valorproducto + domici));
