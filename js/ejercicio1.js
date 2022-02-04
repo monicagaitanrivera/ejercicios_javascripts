279b0764c0677265f2f0091e02fbd176ba390c3d
monicagaitanrivera <riveramonny123@gmail.com>
2022-02-04 10:42:45 -0500

    subiendo test

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -2,8 +2,17 @@
 let mundo = "Hola mundo"
 console.log(mundo);
 
-
-
-
-var cliente= "Monica"
+/* mensaje */ 
+let cliente= "Monica"
 console.log("bienvenido,sr",cliente);
+
+/* Solucion que muestre el total de un producto */ 
+
+let valorproducto = 32000;
+let  impuesto= 0.10;
+let domici =3000;
+
+var costototal= valorproducto + impuesto + domici;
+console.log("sub-total:" + (valorproducto + impuesto));
+console.log("domicilio:"+ domici);
+console.log("El costo total es de: $" + costototal)
