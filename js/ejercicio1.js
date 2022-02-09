65bdfd4b3e5e9ab48625b97e5f71c8f0458606dc
monicagaitanrivera <riveramonny123@gmail.com>
2022-02-09 14:24:14 -0500

    subiendo cambios

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -13,3 +13,20 @@ let  impuesto= .10;
 let domici =3000;
 
 console.log("El costo total es de: $",(( valorproducto * impuesto ) + valorproducto + domici));
+
+/* */
+
+let monto
+let decimal=0.15
+let decimal2= .10
+if(monto <= 30000) {
+    console.log("no tiene descuento");
+
+}
+else if ((monto>30000) && (monto<100000)) {
+    let descuento=monto * decimal
+    console.log("su descuento es de un 5% y su valor total es: ", descuento + monto)
+}
+if (monto>=100000)
+descuento1=(monto * decimal2)
+console.log("su descuento es de un 10% y su valor total es: ", descuento1 + monto);
